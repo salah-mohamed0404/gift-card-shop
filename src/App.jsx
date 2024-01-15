11419669aa52e3d4b95697f271fd60988ae62d18
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-15 16:46:39 +0200

    Setup the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./i18n";
+import { router } from "./router";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+  },
+  {
+    path: "/gift-card",
+  },
+  {
+    path:"/custom-gift-card"
+  },
+  {
+    path:"/checkout"
+  }
+])
