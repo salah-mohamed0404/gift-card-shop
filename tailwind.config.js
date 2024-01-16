96ec1b0ba10de1a51e84b38b67328332c7b63b87
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-16 20:28:24 +0200

    Add custom colors and animations to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+import { violet } from "tailwindcss/colors";
+
 export default {
 	purge: ["./src/**/*.{js,jsx}", "./index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: violet,
+			},
+			transitionDuration: {
+				DEFAULT: "300ms",
+			},
+			animation: {
+				"slide-down": "slide-down 500ms ease-in-out",
+			},
+			keyframes: {
+				"slide-down": {
+					"0%": { transform: "translateY(-100%)" },
+					"100%": { transform: "translateY(0)" },
+				},
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 };
