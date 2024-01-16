89d71791c59e9a73ed7d5ae29a8a257686bd8f34
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-16 15:19:13 +0200

    Refactor main.jsx to use React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import "./i18n";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
