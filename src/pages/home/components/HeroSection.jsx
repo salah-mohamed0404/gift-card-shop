3883029b082e35b22e45acb29993bbe5888d331d
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 13:16:29 +0200

    Add internationalization support to HeroSection component

--- src/pages/home/components/HeroSection.jsx
+++ src/pages/home/components/HeroSection.jsx
@@ -1,14 +1,18 @@
 import { Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export default function HeroSection() {
+	const { t } = useTranslation();
+
 	return (
 		<header className="relative flex flex-col justify-center items-center gap-4 bg-[url('https://source.unsplash.com/random/1920x1080')] min-h-[90dvh] px-2">
 			<div className="absolute inset-0 bg-black opacity-50"></div>
 			<Typography variant="h2" component="h1" className="text-gray-50 z-10">
-				Welcome to our <span className="text-primary-500">gift card shop</span>
+				{t("home.title")}
+				<span className="text-primary-500"> {t("siteTitle")}</span>
 			</Typography>
 			<Typography variant="h5" component="p" className="text-gray-200 z-10">
-				Get a gift card for your loved ones
+				{t("home.paragraph")}
 			</Typography>
 		</header>
 	);
