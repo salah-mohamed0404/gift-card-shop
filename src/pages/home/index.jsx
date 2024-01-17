4685495488abb38514c7207fa5ca7dc71f708882
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 14:21:55 +0200

    Add CardsSection component to Home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
+import CardsSection from "./components/CardsSection";
 import HeroSection from "./components/HeroSection";
 
 export default function Home() {
 	return (
 		<main>
 			<HeroSection />
+			<CardsSection />
 		</main>
 	);
 }
