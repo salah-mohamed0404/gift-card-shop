130e06fc7bb08df625d0404c9be03d548a9bb203
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 14:24:30 +0200

    Add PolicySection component to Home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import CardsSection from "./components/CardsSection";
 import HeroSection from "./components/HeroSection";
+import PolicySection from "./components/PolicySection";
 
 export default function Home() {
 	return (
-		<main>
+		<main className="flex flex-col gap-28">
 			<HeroSection />
 			<CardsSection />
+			<PolicySection />
 		</main>
 	);
 }
