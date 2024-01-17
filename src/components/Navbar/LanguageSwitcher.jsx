b004eacb1675e36c266c1fa6f93691e7e3c0aa5a
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 12:59:11 +0200

    Refactor language switcher component and add useLangSwitch hook

--- src/components/Navbar/LanguageSwitcher.jsx
+++ src/components/Navbar/LanguageSwitcher.jsx
@@ -1,20 +1,15 @@
-import { useTranslation } from "react-i18next";
+import useLangSwitch from "../../hooks/useLangSwitch";
 
 export default function LanguageSwitcher() {
-	const { i18n } = useTranslation();
-
-	const handleLangSwitch = () => {
-		if (i18n.language === "en") i18n.changeLanguage("ar");
-		else i18n.changeLanguage("en");
-	};
+	const { toggleLanguage, language } = useLangSwitch();
 
 	return (
 		<button
 			type="button"
-			onClick={handleLangSwitch}
+			onClick={toggleLanguage}
 			className="transition-colors hover:text-primary-500"
 		>
-			{i18n.language === "en" ? "العربية" : "English"}
+			{language === "en" ? "العربية" : "English"}
 		</button>
 	);
 }
