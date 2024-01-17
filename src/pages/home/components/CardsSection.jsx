b3b79636c9ee2641dcdf45db4b270d9f10350cc7
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 14:21:35 +0200

    Add CardsSection component with custom cards

---
+++ src/pages/home/components/CardsSection.jsx
@@ -0,0 +1,32 @@
+import { useTranslation } from "react-i18next";
+import CustomCard from "../../../components/CustomCard";
+
+export default function CardsSection() {
+	const { t } = useTranslation();
+
+	return (
+		<section>
+			<div className="flex md:flex-row flex-col gap-24 md:w-3/4 w-4/5 mx-auto py-24 px-2">
+				{cards.map((card, index) => (
+					<CustomCard
+						key={`${index}-${card.title}`}
+						imgUrl={card.imgUrl}
+						title={t(`home.cards.${card.title}.title`)}
+						description={t(`home.cards.${card.title}.description`)}
+					/>
+				))}
+			</div>
+		</section>
+	);
+}
+
+const cards = [
+	{
+		imgUrl: "https://source.unsplash.com/random/1000x900",
+		title: "gift",
+	},
+	{
+		imgUrl: "https://source.unsplash.com/random/1000x900",
+		title: "custom",
+	},
+];
