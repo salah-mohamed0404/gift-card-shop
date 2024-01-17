69b3e3fd380c3d456de0b4ab83bdd783d5213125
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 20:00:19 +0200

    Refactor PolicySection component

--- src/pages/home/components/PolicySection.jsx
+++ src/pages/home/components/PolicySection.jsx
@@ -1,28 +1,11 @@
-import { Container, Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
+import CustomTextImgSection from "../../../components/CustomTextImgSection";
 
-export default function PolicySection() {
-	const { t } = useTranslation();
-
+export default function PolicySection({ t }) {
 	return (
-		<Container component="section" className="!flex justify-between flex-wrap">
-			<div className="md:w-6/12 w-full">
-				<img src="https://source.unsplash.com/random/1400x800" alt="policy" />
-			</div>
-
-			<div className="flex flex-col justify-center md:w-5/12 w-full">
-				<Typography
-					variant="h3"
-					component="h2"
-					className="text-primary-700 !mb-6"
-				>
-					<span className="text-gray-700">{t("home.policy.titlePrefix")}</span>{" "}
-					{t("home.policy.title")}
-				</Typography>
-				<Typography variant="h5" component="p">
-					{t("home.policy.description")}
-				</Typography>
-			</div>
-		</Container>
+		<CustomTextImgSection
+			titlePrefix={t("home.policy.titlePrefix")}
+			title={t("home.policy.title")}
+			description={t("home.policy.description")}
+		/>
 	);
 }
