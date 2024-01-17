ef9d7cd2876630bc3978e6cbc8f70b04de857d14
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 14:28:41 +0200

    Refactor CardsSection component to use MUI Container

--- src/pages/home/components/CardsSection.jsx
+++ src/pages/home/components/CardsSection.jsx
@@ -1,22 +1,25 @@
 import { useTranslation } from "react-i18next";
+import { Container } from "@mui/material";
 import CustomCard from "../../../components/CustomCard";
 
 export default function CardsSection() {
 	const { t } = useTranslation();
 
 	return (
-		<section>
-			<div className="flex md:flex-row flex-col gap-24 md:w-3/4 w-4/5 mx-auto px-2">
-				{cards.map((card, index) => (
-					<CustomCard
-						key={`${index}-${card.title}`}
-						imgUrl={card.imgUrl}
-						title={t(`home.cards.${card.title}.title`)}
-						description={t(`home.cards.${card.title}.description`)}
-					/>
-				))}
-			</div>
-		</section>
+		<Container
+			component="section"
+			maxWidth="lg"
+			className="!flex md:flex-row flex-col gap-24"
+		>
+			{cards.map((card, index) => (
+				<CustomCard
+					key={`${index}-${card.title}`}
+					imgUrl={card.imgUrl}
+					title={t(`home.cards.${card.title}.title`)}
+					description={t(`home.cards.${card.title}.description`)}
+				/>
+			))}
+		</Container>
 	);
 }
 
