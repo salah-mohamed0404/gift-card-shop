43f6f02bdc58e21e861b3f7dc5cbe03fc00fcc54
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 13:16:15 +0200

    Add translations for site title, home page, and navbar

--- src/translations/en.js
+++ src/translations/en.js
@@ -1,3 +1,15 @@
 export default {
-	translation: {},
+	translation: {
+		siteTitle: "gift card shop",
+		home: {
+			title: "Welcome to our",
+			paragraph: "Get a gift card for your loved ones",
+		},
+		navbar: {
+			home: "Home",
+			about: "About",
+			"buy a card": "Buy a card",
+			"create a card": "Create a card",
+		},
+	},
 };
