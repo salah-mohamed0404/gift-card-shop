ae9b408217175c0ca61ebb803196740132fcbb8c
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 20:36:19 +0200

    Add Footer component to RootLayout

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,10 +1,12 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
 	return (
 		<>
 			<Navbar />
-      {children}
+			{children}
+			<Footer />
 		</>
 	);
 }
