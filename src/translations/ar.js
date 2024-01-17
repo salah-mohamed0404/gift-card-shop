685f9c6063c7b4060b40f1c643e47bff047d26e9
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 13:16:10 +0200

    Update Arabic translation for siteTitle, home, and navbar

--- src/translations/ar.js
+++ src/translations/ar.js
@@ -1,3 +1,15 @@
 export default {
-	translation: {},
+	translation: {
+		siteTitle: "متجر بطاقات الهدايا",
+		home: {
+			title: "مرحبا بكم في",
+			paragraph: "احصل على بطاقة هدايا لأحبائك",
+		},
+		navbar: {
+			home: "الرئيسية",
+			about: "عنا",
+			"buy a card": "اشتري بطاقة",
+			"create a card": "أنشئ بطاقة",
+		},
+	},
 };
