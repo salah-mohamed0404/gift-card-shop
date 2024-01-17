a4eea615e3a8b508edce50a7c4cccee26773345d
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-17 20:57:52 +0200

    Refactor CartOpenBtn component to include type attribute in IconButton

--- src/components/Navbar/Cart/CartOpenBtn.jsx
+++ src/components/Navbar/Cart/CartOpenBtn.jsx
@@ -3,7 +3,11 @@ import { IconButton } from "@mui/material";
 
 export default function CartOpenBtn({ onOpenDrawer }) {
 	return (
-		<IconButton onClick={onOpenDrawer} className="!text-inherit !text-3xl">
+		<IconButton
+			type="button"
+			onClick={onOpenDrawer}
+			className="!text-inherit !text-3xl"
+		>
 			<ShoppingBag fontSize="inherit" />
 		</IconButton>
 	);
