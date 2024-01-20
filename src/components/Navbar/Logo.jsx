4475a6b22cb9aee74802eb45d6a623738871c1d2
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-20 13:54:04 +0200

    Update logo image source and dimensions

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Logo() {
 	return (
 		<Link to="/">
-			<img alt="logo" width={62} height={63} />
+			<img src="/images/logo.webp" alt="logo" width={128} height={128} />
 		</Link>
 	);
 }
