63a0c6954d7d6cb532a3f81c29f492f913b450ce
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-20 13:54:29 +0200

    Add image to AboutSection component

--- src/pages/Home/components/AboutSection.jsx
+++ src/pages/Home/components/AboutSection.jsx
@@ -3,6 +3,7 @@ import CustomTextImgSection from "../../../components/CustomTextImgSection";
 export default function AboutSection({ t }) {
 	return (
 		<CustomTextImgSection
+			imgUrl="/images/about.webp"
 			titlePrefix={t("home.about.titlePrefix")}
 			title={t("home.about.title")}
 			description={t("home.about.description")}
