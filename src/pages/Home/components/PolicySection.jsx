373c0b1129a1b8a61c20fc83eed72552a6e063d1
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-20 13:54:49 +0200

    Add policy image to PolicySection component

--- src/pages/Home/components/PolicySection.jsx
+++ src/pages/Home/components/PolicySection.jsx
@@ -3,6 +3,7 @@ import CustomTextImgSection from "../../../components/CustomTextImgSection";
 export default function PolicySection({ t }) {
 	return (
 		<CustomTextImgSection
+			imgUrl="/images/policy.webp"
 			titlePrefix={t("home.policy.titlePrefix")}
 			title={t("home.policy.title")}
 			description={t("home.policy.description")}
