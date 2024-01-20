1f3fddaf7dbb94bb9871ec236259dfabcdd674ae
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-20 13:54:44 +0200

    Update HeroSection styles and typography

--- src/pages/Home/components/HeroSection.jsx
+++ src/pages/Home/components/HeroSection.jsx
@@ -2,13 +2,21 @@ import { Typography } from "@mui/material";
 
 export default function HeroSection({ t }) {
 	return (
-		<header className="relative flex flex-col justify-center items-center gap-4 bg-[url('https://source.unsplash.com/random/1920x1080')] bg-cover min-h-[90dvh] px-2">
+		<header className="relative flex flex-col justify-center items-center gap-4 bg-hero-background bg-cover bg-center min-h-[90dvh] px-2">
 			<div className="absolute inset-0 bg-black opacity-50"></div>
-			<Typography variant="h2" component="h1" className="text-gray-50 z-10">
+			<Typography
+				variant="h2"
+				component="h1"
+				className="text-gray-50 capitalize z-10"
+			>
 				{t("home.hero.title")}
 				<span className="text-primary-500"> {t("siteTitle")}</span>
 			</Typography>
-			<Typography variant="h5" component="p" className="text-gray-200 z-10">
+			<Typography
+				variant="h5"
+				component="p"
+				className="text-gray-200 capitalize z-10"
+			>
 				{t("home.hero.description")}
 			</Typography>
 		</header>
