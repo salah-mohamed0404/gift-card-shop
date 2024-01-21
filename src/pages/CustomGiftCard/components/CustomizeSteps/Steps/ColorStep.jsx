0141e7fb127c5910115e8f4f452bf2b271ccc947
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-21 21:26:18 +0200

    Add ColorStep component for customizing card color

---
+++ src/pages/CustomGiftCard/components/CustomizeSteps/Steps/ColorStep.jsx
@@ -0,0 +1,39 @@
+import { Typography } from "@mui/material";
+import colors from "tailwindcss/colors";
+
+export default function ColorStep({ color, onColorChange }) {
+	return (
+		<div className="flex flex-col items-center gap-6">
+			<Typography variant="h4" className="capitalize">
+				choose card color
+			</Typography>
+
+			<ul className="flex justify-center gap-6 flex-wrap">
+				{CardColors.map((cardColor) => (
+					<li key={cardColor}>
+						<button
+							className={`w-9 h-9 rounded-full ${
+								cardColor === color
+									? "outline-none ring-2 ring-offset-8 ring-red-500"
+									: ""
+							}`}
+							style={{ backgroundColor: cardColor }}
+							onClick={() => onColorChange(cardColor)}
+						/>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+const CardColors = [
+	...new Set(
+		Object.values(colors)
+			.filter((value) => value[200])
+			.reduce((acc, value) => {
+				const colorValues = [200, 500, 800].map((grade) => value[grade]);
+				return [...acc, ...colorValues];
+			}, [])
+	),
+];
