6c1abe777899d104ab8d70cea353d112e73ce488
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-21 21:25:56 +0200

    Refactor CustomGiftCard component to include CardPreview and CustomizeSteps components

--- src/pages/CustomGiftCard/index.jsx
+++ src/pages/CustomGiftCard/index.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import CardPreview from "./components/CardPreview";
+import CustomizeSteps from "./components/CustomizeSteps";
+import useCardSitting from "./hooks/useCardSitting";
+
 export default function CustomGiftCard() {
+	const { cardSitting, dispatchCardSitting } = useCardSitting();
+	const [activeStep, setActiveStep] = useState(0);
+
 	return (
-		<div>
-			<h1>custom gift card</h1>
-		</div>
+		<main className="mt-36 mb-28">
+			<div className="flex items-center gap-16 w-3/4 mx-auto">
+				<CardPreview cardSitting={cardSitting} back={activeStep === 3} />
+				<CustomizeSteps
+					cardSitting={cardSitting}
+					dispatchCardSitting={dispatchCardSitting}
+					activeStep={activeStep}
+					onActiveStepChange={setActiveStep}
+				/>
+			</div>
+		</main>
 	);
 }
