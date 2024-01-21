bab2873f3e9d615e63aeb6e0e3768a7769042e21
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-21 21:26:34 +0200

    Add ShapeStep component for customizing card shape

---
+++ src/pages/CustomGiftCard/components/CustomizeSteps/Steps/ShapeStep.jsx
@@ -0,0 +1,57 @@
+import { Typography } from "@mui/material";
+
+export default function ShapeStep({ shape, onShapeChange, color }) {
+	return (
+		<div className="flex flex-col items-center gap-6">
+			<Typography variant="h4" className="capitalize">
+				choose card shape
+			</Typography>
+
+			<ul className="grid md:grid-cols-2 justify-center gap-6 flex-wrap">
+				{shapes.map((cardShape) => (
+					<li
+						key={cardShape}
+						className={`overflow-hidden rounded-xl shadow-lg ${
+							cardShape === shape
+								? "outline-none ring-2 ring-offset-8 ring-red-500"
+								: ""
+						}`}
+						style={{
+							backgroundColor: color,
+						}}
+					>
+						<button type="button" onClick={() => onShapeChange(cardShape)}>
+							<img
+								src={cardShape}
+								alt="shape"
+								className="w-60 h-36 object-cover"
+							/>
+						</button>
+					</li>
+				))}
+
+				<li
+					className={`overflow-hidden rounded-xl shadow-lg ${
+						!shape ? "outline-none ring-2 ring-offset-[10px] ring-red-500" : ""
+					}`}
+					style={{
+						backgroundColor: color,
+					}}
+				>
+					<button
+						type="button"
+						className="w-60 h-36"
+						onClick={() => onShapeChange(null)}
+					></button>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+const shapes = [
+	"/images/card-shapes/stars-1.png",
+	"/images/card-shapes/ribbon-1.png",
+	"/images/card-shapes/ribbon-2.png",
+	"/images/card-shapes/stars-2.png",
+];
