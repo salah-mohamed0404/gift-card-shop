49acb5b8c3beb316014ee961d1366c81d139deac
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-21 19:59:35 +0200

    Add MUI theme to RootLayout

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,12 +1,22 @@
+import { ThemeProvider, createTheme } from "@mui/material";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { blue } from "@mui/material/colors";
 
 export default function RootLayout({ children }) {
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: blue[500],
+			},
+		},
+	});
+
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<Navbar />
 			{children}
 			<Footer />
-		</>
+		</ThemeProvider>
 	);
 }
