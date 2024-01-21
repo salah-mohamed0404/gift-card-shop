b40f5e792b1bfbfa289c01c95a15401681af68a1
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-21 21:26:22 +0200

    Add steps component for customizing gift card

---
+++ src/pages/CustomGiftCard/components/CustomizeSteps/Steps/index.jsx
@@ -0,0 +1,74 @@
+import { cardSittingActions } from "../../../hooks/useCardSitting";
+import BrandStep from "./BrandStep";
+import ColorStep from "./ColorStep";
+import MessageAndPriceStep from "./MessageAndPriceStep";
+import ShapeStep from "./ShapeStep";
+
+export default function Steps({
+	cardSitting,
+	dispatchCardSitting,
+	activeStep,
+}) {
+	return (
+		<div>
+			{activeStep === 0 && (
+				<ColorStep
+					color={cardSitting.color}
+					onColorChange={(color) =>
+						dispatchCardSitting({
+							type: cardSittingActions.SET_COLOR,
+							payload: color,
+						})
+					}
+				/>
+			)}
+			{activeStep === 1 && (
+				<ShapeStep
+					shape={cardSitting.shape}
+					color={cardSitting.color}
+					onShapeChange={(shape) =>
+						dispatchCardSitting({
+							type: cardSittingActions.SET_SHAPE,
+							payload: shape,
+						})
+					}
+				/>
+			)}
+			{activeStep === 2 && (
+				<BrandStep
+					brand={cardSitting.brand}
+					onBrandChange={(brand) =>
+						dispatchCardSitting({
+							type: cardSittingActions.SET_BRAND,
+							payload: brand,
+						})
+					}
+				/>
+			)}
+			{activeStep === 3 && (
+				<MessageAndPriceStep
+					message={cardSitting.message}
+					price={cardSitting.price}
+					onMessageChange={(message) =>
+						dispatchCardSitting({
+							type: cardSittingActions.SET_MESSAGE,
+							payload: message,
+						})
+					}
+					onPriceChange={(price) =>
+						dispatchCardSitting({
+							type: cardSittingActions.SET_PRICE,
+							payload: price,
+						})
+					}
+					onTextColorChange={(textColor) =>
+						dispatchCardSitting({
+							type: cardSittingActions.SET_TEXT_COLOR,
+							payload: textColor,
+						})
+					}
+				/>
+			)}
+		</div>
+	);
+}
