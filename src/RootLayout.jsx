9abea4c96d2740f37af379e557841cbdb2aa245a
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-22 15:05:26 +0200

    Add language support and direction based on user selection

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -2,8 +2,13 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { blue } from "@mui/material/colors";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function RootLayout({ children }) {
+	const {
+		i18n: { language },
+	} = useTranslation();
 	const theme = createTheme({
 		palette: {
 			primary: {
@@ -11,6 +16,11 @@ export default function RootLayout({ children }) {
 			},
 		},
 	});
+
+	useEffect(() => {
+		document.documentElement.lang = language;
+		document.documentElement.dir = language === "ar" ? "rtl" : "ltr";
+	}, [language]);
 
 	return (
 		<ThemeProvider theme={theme}>
