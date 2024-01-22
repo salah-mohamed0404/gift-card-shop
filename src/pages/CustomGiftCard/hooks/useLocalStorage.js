3d3621d57b603fb2c22188e61c0b9dbd249c07cc
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-22 13:12:39 +0200

    Add useLocalStorage hook for storing and retrieving data in local storage

---
+++ src/pages/CustomGiftCard/hooks/useLocalStorage.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+/**
+ * Custom hook for storing and retrieving data in local storage.
+ *
+ * @param {string} key - The key to use for storing the data in local storage.
+ * @param {*} initialValue - The initial value to use if no data is found in local storage.
+ * @returns {[value: *, setValue: Function]} An array that correspond to the localStorage key where:
+ * - The first element, `value`, is the stored value.
+ * - The second element, `setValue`, is a function that can be called to update the localStorage stored value .
+ */
+export default function useLocalStorage(key, initialValue) {
+	const [storedValue, setStoredValue] = useState(initialValue);
+
+	useEffect(() => {
+		try {
+			const item = localStorage.getItem(key);
+			setStoredValue(item ? JSON.parse(item) : initialValue);
+		} catch (error) {
+			console.error(error);
+			setStoredValue(initialValue);
+		}
+	}, [key, initialValue]);
+
+	const setValue = (value) => {
+		try {
+			const valueToStore =
+				value instanceof Function ? value(storedValue) : value;
+			setStoredValue(valueToStore);
+			localStorage.setItem(key, JSON.stringify(valueToStore));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	return [storedValue, setValue];
+}
