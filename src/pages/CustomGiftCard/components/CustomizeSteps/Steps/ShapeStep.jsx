0d3eb4b37c1d19d20e5d30fbb88742ac1d4ac0ed
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-23 21:19:55 +0200

    Update typography variant and styles in ShapeStep component

--- src/pages/CustomGiftCard/components/CustomizeSteps/Steps/ShapeStep.jsx
+++ src/pages/CustomGiftCard/components/CustomizeSteps/Steps/ShapeStep.jsx
@@ -3,7 +3,11 @@ import { Typography } from "@mui/material";
 export default function ShapeStep({ t, shape, onShapeChange, color }) {
 	return (
 		<div className="flex flex-col items-center gap-6">
-			<Typography variant="h4" className="capitalize">
+			<Typography
+				variant="h4"
+				className="md:!text-4xl !text-3xl capitalize"
+				textAlign="center"
+			>
 				{t("customCard.shape.title")}
 			</Typography>
 
