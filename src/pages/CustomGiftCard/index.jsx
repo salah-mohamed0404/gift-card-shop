b7145fe5419d4fc9a4663d549e45684e82b7c254
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-23 21:19:25 +0200

    Refactor CustomGiftCard component layout

--- src/pages/CustomGiftCard/index.jsx
+++ src/pages/CustomGiftCard/index.jsx
@@ -8,8 +8,8 @@ export default function CustomGiftCard() {
 	const [activeStep, setActiveStep] = useState(0);
 
 	return (
-		<main className=" flex items-center mt-36 mb-28 min-h-[60dvh]">
-			<div className="flex items-center gap-16 w-3/4 mx-auto">
+		<main className=" flex items-center mt-36 md:mb-28 mb-16 md:min-h-[60dvh]">
+			<div className="flex md:flex-row flex-col items-center gap-16 w-3/4 mx-auto">
 				<CardPreview cardSitting={cardSitting} back={activeStep === 3} />
 				<CustomizeSteps
 					cardSitting={cardSitting}
