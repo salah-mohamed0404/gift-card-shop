1fb51844dd05d095723f80b702b7bc07ca141328
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-23 11:48:09 +0200

    Refactor CustomStepper component to use translation for step labels

--- src/pages/CustomGiftCard/components/CustomizeSteps/CustomStepper.jsx
+++ src/pages/CustomGiftCard/components/CustomizeSteps/CustomStepper.jsx
@@ -1,14 +1,27 @@
 import { Step, StepLabel, Stepper } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
-const steps = ["Color", "Shape", "Brand", "Message And Price", "Receiver Info"];
+const steps = ["color", "shape", "brand", "message", "receiverInfo"];
 
 export default function CustomStepper({ activeStep }) {
+	const { t } = useTranslation();
+
 	return (
 		<div className="flex justify-center">
-			<Stepper activeStep={activeStep} alternativeLabel className="w-full">
+			<Stepper
+				activeStep={activeStep}
+				alternativeLabel
+				className="w-full"
+				sx={{
+					"& .MuiStepConnector-root": {
+						insetInlineStart: "calc(-50% + 20px)",
+						insetInlineEnd: "calc(50% + 20px)",
+					},
+				}}
+			>
 				{steps.map((label) => (
 					<Step key={label}>
-						<StepLabel>{label}</StepLabel>
+						<StepLabel>{t(`customCard.steps.${label}`)}</StepLabel>
 					</Step>
 				))}
 			</Stepper>
