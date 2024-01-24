a6c52972e64a18536a2f9156455fd8cb0f08c963
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-24 09:08:34 +0200

    Update GiftCard component layout

--- src/pages/GiftCard.jsx
+++ src/pages/GiftCard.jsx
@@ -1,7 +1,16 @@
 export default function GiftCard() {
 	return (
-		<div>
-			<h1>GiftCard</h1>
-		</div>
+		<main className="mt-36 md:mb-28 mb-16">
+			<div className="w-3/4 mx-auto">
+				<h1 className="text-4xl font-medium text-center">
+					<span className="text-primary-500">Ready cards</span> for you
+				</h1>
+
+				<div>
+					<div></div>
+					<ul></ul>
+				</div>
+			</div>
+		</main>
 	);
 }
