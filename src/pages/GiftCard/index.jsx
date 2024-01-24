282dc8f6d8646fa92895a37d2c65e03b5b2e5816
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-24 17:50:51 +0200

    Update GiftCard component with pagination

--- src/pages/GiftCard/index.jsx
+++ src/pages/GiftCard/index.jsx
@@ -1,28 +1,32 @@
 import { useTranslation } from "react-i18next";
 import CardItem from "./CardItem";
+import CardPagination from "./CardPagination";
 
 export default function GiftCard() {
 	const { t } = useTranslation();
 
 	return (
-		<main className="mt-40 md:mb-28 mb-16">
+		<main className="mt-40 md:mb-20 mb-16">
 			<div className="w-3/4 mx-auto">
 				<h1 className="md:text-4xl text-2xl font-medium text-center mb-20">
 					<span className="text-primary-500">{t("readyCards.title")}</span>{" "}
 					{t("readyCards.titleSuffix")}
 				</h1>
 
-				<div>
-					<div className="flex justify-between items-center mb-6 pb-2 border-b-2 text-lg">
-						<p>{t("readyCards.results", { count: cards.length })}</p>
-					</div>
-					<ul className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-10 gap-y-6">
-						{cards.map((card, index) => (
-							<li key={`${card.brand}-${index}`}>
-								<CardItem card={card} t={t} />
-							</li>
-						))}
-					</ul>
+				<div className="flex justify-between items-center mb-6 pb-2 border-b-2 text-lg">
+					<p>{t("readyCards.results", { count: cards.length })}</p>
+				</div>
+
+				<ul className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-10 gap-y-6">
+					{cards.map((card, index) => (
+						<li key={`${card.brand}-${index}`}>
+							<CardItem card={card} t={t} />
+						</li>
+					))}
+				</ul>
+
+				<div className="grid place-content-center mt-8">
+					<CardPagination />
 				</div>
 			</div>
 		</main>
