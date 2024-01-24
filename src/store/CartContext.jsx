cfec05780a57291aca5d954aa2133d0e62c4dd5a
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-24 09:08:50 +0200

    Refactor CartContext.jsx to use setStoredCart in reducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,34 +1,35 @@
 import { createContext, useEffect, useReducer } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
-// Define the context
 export const CartContext = createContext();
 
-// Define the initial state of the cart
-const initialCart = [];
-
-// Define the reducer
 const cartReducer = (state, action) => {
+	const { setStoredCart, payload } = action;
+
 	switch (action.type) {
 		case "ADD_ITEM":
-			return [...state, action.payload];
+			return setStoredCart([...state, payload]);
 		case "REMOVE_ITEM":
-			return state.filter((_, index) => index !== action.payload);
+			return setStoredCart(state.filter((_, index) => index !== payload));
+		case "INIT":
+			return action.payload;
 		default:
 			return state;
 	}
 };
 
-// Define the provider
 export const CartProvider = ({ children }) => {
-	const [storedCart, setStoredCart] = useLocalStorage("cart", initialCart);
-	const [cart, dispatchCart] = useReducer(cartReducer, storedCart);
+	const [storedCart, setStoredCart] = useLocalStorage("cart", []);
+	const [cart, dispatch] = useReducer(cartReducer, storedCart);
 
 	useEffect(() => {
-		setStoredCart(cart);
-	}, [cart, setStoredCart]);
+		dispatch({ type: "INIT", payload: storedCart });
+	}, [storedCart]);
 
-	console.log(cart);
+	const dispatchCart = (action) => {
+		action.setStoredCart = setStoredCart;
+		dispatch(action);
+	};
 
 	return (
 		<CartContext.Provider value={{ cart, dispatchCart }}>
