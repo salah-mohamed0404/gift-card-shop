780e61fbf91faea6af306342a03773900365aff6
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-24 16:46:53 +0200

    Update GiftCard title font size

--- src/pages/GiftCard/index.jsx
+++ src/pages/GiftCard/index.jsx
@@ -7,7 +7,7 @@ export default function GiftCard() {
 	return (
 		<main className="mt-40 md:mb-28 mb-16">
 			<div className="w-3/4 mx-auto">
-				<h1 className="text-4xl font-medium text-center mb-20">
+				<h1 className="md:text-4xl text-2xl font-medium text-center mb-20">
 					<span className="text-primary-500">{t("readyCards.title")}</span>{" "}
 					{t("readyCards.titleSuffix")}
 				</h1>
