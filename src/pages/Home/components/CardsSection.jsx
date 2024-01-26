bfc31026dc6225369b2390bf13c85ac5053af2e2
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-26 16:09:50 +0200

    Refactor CardsSection component

--- src/pages/Home/components/CardsSection.jsx
+++ src/pages/Home/components/CardsSection.jsx
@@ -2,39 +2,44 @@ import { Container, Typography, Grid } from "@mui/material";
 import CustomCard from "../../../components/CustomCard";
 
 export default function CardsSection({ t }) {
-  return (
-    <Container component="section">
-      <Typography
-        variant="h3"
-        component="h2"
-        className="text-primary-700 text-center capitalize !mb-10"
-      >
-        - <span className="text-gray-700">{t("home.cards.titlePrefix")}</span>{" "}
-        {t("home.cards.title")} -
-      </Typography>
+	return (
+		<Container component="section">
+			<Typography
+				variant="h3"
+				component="h2"
+				className="text-primary-700 text-center capitalize !mb-10"
+			>
+				- <span className="text-gray-700">{t("home.cards.titlePrefix")}</span>{" "}
+				{t("home.cards.title")} -
+			</Typography>
 
-      <Grid container spacing={3}>
-        {cards.map((card, index) => (
-          <Grid item xs={12} sm={6} key={`${index}-${card.title}`}>
-            <CustomCard
-              imgUrl={card.imgUrl}
-              title={t(`home.cards.${card.title}.title`)}
-              description={t(`home.cards.${card.title}.description`)}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
-  );
+			<Grid
+				container
+				spacing={3}
+				justifyContent="space-between"
+				className="px-8"
+			>
+				{cards.map((card, index) => (
+					<Grid item xs={12} sm={5} key={`${index}-${card.title}`}>
+						<CustomCard
+							imgUrl={card.imgUrl}
+							title={t(`home.cards.${card.title}.title`)}
+							description={t(`home.cards.${card.title}.description`)}
+						/>
+					</Grid>
+				))}
+			</Grid>
+		</Container>
+	);
 }
 
 const cards = [
-  {
-    imgUrl: "/images/home1.png",
-    title: "gift",
-  },
-  {
-    imgUrl: "/images/home2.png",
-    title: "custom",
-  },
+	{
+		imgUrl: "/images/home1.png",
+		title: "gift",
+	},
+	{
+		imgUrl: "/images/home2.png",
+		title: "custom",
+	},
 ];
