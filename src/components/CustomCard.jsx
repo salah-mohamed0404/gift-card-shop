f1ed579a645569806950eee66da8202230a615d2
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-26 16:09:46 +0200

    Refactor CustomCard component styles

--- src/components/CustomCard.jsx
+++ src/components/CustomCard.jsx
@@ -3,18 +3,28 @@ import { Link } from "react-router-dom";
 
 export default function CustomCard({ imgUrl, title, description }) {
 	return (
-		<Card className=" text-center " sx={{boxShadow:' rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px' }}>
+		<Card
+			className="text-center !rounded-2xl"
+			sx={{
+				boxShadow:
+					" rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+			}}
+		>
 			<Link to="/gift-card">
 				<CardMedia
-					sx={{ height: 260, objectFit: "cover"}}
+					sx={{ height: 260, objectFit: "cover" }}
 					image={imgUrl}
 					title={title}
+					className="!rounded-2xl"
 				/>
 			</Link>
 
 			<CardContent>
 				<Link to="/gift-card">
-					<Typography variant="h4" className="text-primary-700 sm:fs-5 !mb-1 sm-mt-4 sm-text-center" >
+					<Typography
+						variant="h4"
+						className="text-primary-700 sm:fs-5 !mb-1 sm-mt-4 sm-text-center"
+					>
 						{title}
 					</Typography>
 				</Link>
