0410bf52c90da22be5d8dadafb3fb41826382f15
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-27 16:10:42 +0200

    Update HeroSection component styles

--- src/pages/Home/components/HeroSection.jsx
+++ src/pages/Home/components/HeroSection.jsx
@@ -1,8 +1,6 @@
-import { Typography } from "@mui/material";
-
-export default function HeroSection({ t }) {
+export default function HeroSection() {
 	return (
-		<header className="relative flex flex-col justify-center items-center gap-4 bg-hero-background bg-cover bg-center min-h-[90vh] px-2">
+		<header className="relative flex flex-col justify-center items-center gap-4 bg-hero-background bg-cover bg-center md:min-h-[90vh] min-h-[40vh] px-2">
 			<div className="absolute inset-0 bg-black opacity-0"></div>
 		</header>
 	);
