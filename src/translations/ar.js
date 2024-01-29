5b0cc1a1717c0ae4286d0a6704b46f42222a9ac4
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-29 17:12:16 +0200

    Update translations

--- src/translations/ar.js
+++ src/translations/ar.js
@@ -1,124 +1,125 @@
 export default {
-  translation: {
-    siteTitle: "اهد هدية",
-    currency: "ر.س",
-    navbar: {
-      home: "الرئيسية",
-      about: "عنا",
-      "buy a card": "اشتري بطاقة",
-      "create a card": "أنشئ بطاقة",
-      stores: "المتاجر",
-    },
-    home: {
-      hero: {
-        title: "اهدى اختيار",
-        description: "عندما تصبح الهدية شخصية",
-      },
-      cards: {
-        titlePrefix: "اختار",
-        title: "ما يناسبك",
-        gift: {
-          title: "بطاقات الهدايا",
-          description: "اختر بطاقة هدايا من مجموعتنا الواسعة",
-        },
-        custom: {
-          title: "بطاقات مخصصة",
-          description: "أنشئ بطاقة هدايا مخصصة لأحبائك",
-        },
-      },
-      policy: {
-        titlePrefix: "سياسة",
-        title: "المتجر",
-        description:
-          "نحن نقدم مجموعة واسعة من بطاقات الهدايا التي تناسب جميع الأذواق والمناسبات. نحن نقدم أيضًا إمكانية إنشاء بطاقة هدايا مخصصة بتصميمك ونصك الخاص.",
-      },
-      about: {
-        titlePrefix: "عن",
-        title: "المتجر",
-        description:
-          "متجر بطاقات الهدايا هو متجر إلكتروني يقدم بطاقات هدايا مخصصة للمناسبات المختلفة. يمكنك اختيار بطاقة هدايا من مجموعتنا الواسعة أو إنشاء بطاقة هدايا مخصصة بتصميمك ونصك الخاص.",
-      },
-    },
-    customCard: {
-      steps: {
-        color: "اللون",
-        shape: "الشكل",
-        brand: "المتجر",
-        message: "الرسالة والسعر",
-        receiverInfo: "معلومات المستلم",
-      },
-      color: {
-        title: "اختر لون",
-      },
-      shape: {
-        title: "اختر شكل",
-      },
-      brand: {
-        title: "اختر متجر",
-      },
-      message: {
-        title: "املأ الرسالة والسعر",
-        message: "الرسالة",
-        price: "السعر",
-      },
-      receiverInfo: {
-        title: "املأ معلومات المستلم",
-        name: "الاسم",
-        phone: "رقم الهاتف",
-      },
-      addToCart: "أضف إلى السلة",
-    },
-    cart: {
-      title: "السلة",
-      empty: "السلة فارغة",
-      checkout: "الدفع",
-      customCard: "بطاقة مخصصة",
-      readyCard: "بطاقة جاهزة",
-      receiverInfo: "معلومات المستلم",
-      brand: "المتجر",
-      price: "السعر",
-    },
-    readyCards: {
-      title: "بطاقات جاهزة",
-      titleSuffix: "لك",
-      results: "النتائج ({{count}})",
-      modalTitle: "أضف البطاقة إلى السلة",
-      name: "الاسم",
-      phone: "رقم الهاتف",
-      addToCart: "أضف إلى السلة",
-      filters: {
-        button: "الفلاتر",
-        title: "الفلاتر",
-        brands: "المتاجر",
-        price: "السعر",
-        apply: "تطبيق",
-      },
-    },
-    preview: {
-      front: "الوجه الأمامي",
-      back: "الوجه الخلفي",
-    },
-    addToCartSuccess: "تمت إضافة البطاقة إلى السلة بنجاح",
-    stores: {
-      title: "المتاجر",
-      titleSuffix: "التي نعمل معها",
-    },
-    checkoutForm: {
-      name: "الاسم",
-      phone: "الهاتف",
-      email: "البريد الإلكتروني",
-      address: "العنوان",
-      city: "المدينة",
-      zip: "الرمز البريدي",
-      cardNumber: "رقم البطاقة",
-      cvv: "CVV",
-      expiryDate: "تاريخ الانتهاء (شهر/سنة)",
-      submit: "إرسال",
-      amount: "المبلغ",
-      cancel: "إلغاء",
-      mobileCode: "966",
-      invoice: "الفاتورة",
-      pay: "دفع",
-    },
-  },
+	translation: {
+		siteTitle: "اهد هدية",
+		currency: "ر.س",
+		navbar: {
+			home: "الرئيسية",
+			about: "عنا",
+			"buy a card": "اشتري بطاقة",
+			"create a card": "أنشئ بطاقة",
+			stores: "المتاجر",
+		},
+		home: {
+			hero: {
+				title: "اهدى اختيار",
+				description: "عندما تصبح الهدية شخصية",
+			},
+			cards: {
+				titlePrefix: "اختار",
+				title: "ما يناسبك",
+				gift: {
+					title: "بطاقات الهدايا",
+					description: "اختر بطاقة هدايا من مجموعتنا الواسعة",
+				},
+				custom: {
+					title: "بطاقات مخصصة",
+					description: "أنشئ بطاقة هدايا مخصصة لأحبائك",
+				},
+			},
+			policy: {
+				titlePrefix: "سياسة",
+				title: "المتجر",
+				description:
+					"نحن نقدم مجموعة واسعة من بطاقات الهدايا التي تناسب جميع الأذواق والمناسبات. نحن نقدم أيضًا إمكانية إنشاء بطاقة هدايا مخصصة بتصميمك ونصك الخاص.",
+			},
+			about: {
+				titlePrefix: "عن",
+				title: "المتجر",
+				description:
+					"متجر بطاقات الهدايا هو متجر إلكتروني يقدم بطاقات هدايا مخصصة للمناسبات المختلفة. يمكنك اختيار بطاقة هدايا من مجموعتنا الواسعة أو إنشاء بطاقة هدايا مخصصة بتصميمك ونصك الخاص.",
+			},
+		},
+		customCard: {
+			steps: {
+				color: "اللون",
+				shape: "الشكل",
+				brand: "المتجر",
+				message: "الرسالة والسعر",
+				receiverInfo: "معلومات المستلم",
+			},
+			color: {
+				title: "اختر لون",
+			},
+			shape: {
+				title: "اختر شكل",
+			},
+			brand: {
+				title: "اختر متجر",
+			},
+			message: {
+				title: "املأ الرسالة والسعر",
+				message: "الرسالة",
+				price: "السعر",
+			},
+			receiverInfo: {
+				title: "املأ معلومات المستلم",
+				name: "الاسم",
+				phone: "رقم الهاتف",
+			},
+			addToCart: "أضف إلى السلة",
+		},
+		cart: {
+			title: "السلة",
+			empty: "السلة فارغة",
+			checkout: "الدفع",
+			customCard: "بطاقة مخصصة",
+			readyCard: "بطاقة جاهزة",
+			receiverInfo: "معلومات المستلم",
+			brand: "المتجر",
+			price: "السعر",
+		},
+		readyCards: {
+			title: "بطاقات جاهزة",
+			titleSuffix: "لك",
+			results: "النتائج ({{count}})",
+			modalTitle: "أضف البطاقة إلى السلة",
+			name: "الاسم",
+			phone: "رقم الهاتف",
+			addToCart: "أضف إلى السلة",
+			filters: {
+				button: "الفلاتر",
+				title: "الفلاتر",
+				brands: "المتاجر",
+				price: "السعر",
+				apply: "تطبيق",
+			},
+		},
+		preview: {
+			front: "الوجه الأمامي",
+			back: "الوجه الخلفي",
+		},
+		addToCartSuccess: "تمت إضافة البطاقة إلى السلة بنجاح",
+		stores: {
+			title: "المتاجر",
+			titleSuffix: "التي نعمل معها",
+		},
+		checkoutForm: {
+			name: "الاسم",
+			phone: "الهاتف",
+			email: "البريد الإلكتروني",
+			address: "العنوان",
+			city: "المدينة",
+			zip: "الرمز البريدي",
+			cardNumber: "رقم البطاقة",
+			cvv: "CVV",
+			expiryDate: "تاريخ الانتهاء (شهر/سنة)",
+			submit: "إرسال",
+			amount: "المبلغ",
+			cancel: "إلغاء",
+			mobileCode: "966",
+			invoice: "الفاتورة",
+			pay: "دفع",
+			schedule: "جدولة",
+		},
+	},
 };
