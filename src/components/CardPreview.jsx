8f9b8afcbd058e0dc4981802a1c9439b4fc85d70
Salah Mohamed <salah.mohamed0404@gmail.com>
2024-01-30 10:38:07 +0200

    Refactor card preview and shape selection

--- src/components/CardPreview.jsx
+++ src/components/CardPreview.jsx
@@ -15,7 +15,7 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
 	return (
 		<div className="w-full lg:w-[30rem] sm:px-[32px]  aspect-[2/1]  ">
 			<figure
-				className="relative w-[260px] md:w-[30rem] w-full h-full shrink-0   rounded-2xl  overflow-hidden transition-colors"
+				className="relative md:w-[30rem] w-full h-full shrink-0   rounded-2xl  overflow-hidden transition-colors"
 				style={{ backgroundColor: cardSitting.color }}
 			>
 				{!back ? (
@@ -25,7 +25,7 @@ export default function CardPreview({ cardSitting, back, ShowBrand }) {
 								<img
 									src={cardSitting.shape}
 									alt="gift card"
-									className="w-full h-full object-fill"
+									className="w-full h-full object-cover"
 								/>
 							) : null}
 						</div>
